29bd3eaafbc88857b94edcf86a30a474ac37a2f4
harshrajdebug <harshrajdebug@users.noreply.github.com>
2026-09-20 18:37:21 +0000

    add solution for reverse_linked_list

---
+++ linked-list/reverse_linked_list.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def solve(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        hard | LC #1755
+        Approach: two pointer - slow and fast
+        """
+        prev = None
+        curr = head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
+
+if __name__ == "__main__":
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    sol = Solution()
+    res = sol.solve(head)
+    while res:
+        print(res.val, end=" ")
+        res = res.next
