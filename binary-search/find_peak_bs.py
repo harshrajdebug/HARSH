141a648ca71ffd0c52f97de2e33bef6356ccf4d2
harshrajdebug <harshrajdebug@users.noreply.github.com>
2026-09-21 20:53:45 +0000

    solved find_peak_bs #1906

---
+++ binary-search/find_peak_bs.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def solve(self, nums: List[int]) -> int:
+        """
+        hard | LC #1906
+        """
+        if not nums:
+            return 0
+        seen = set()
+        longest = 0
+        for n in nums:
+            if n - 1 not in seen:
+                curr = n
+                streak = 1
+                while curr + 1 in seen:
+                    curr += 1
+                    streak += 1
+                longest = max(longest, streak)
+            seen.add(n)
+        return longest
+
+
+if __name__ == "__main__":
+    print(Solution().solve([100, 4, 200, 1, 3, 2]))
